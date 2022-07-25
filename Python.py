bd625e4fb405df7de800ce039e78454e3815ddc7
Cristhian-21 <109809124+Cristhian-21@users.noreply.github.com>
2022-07-25 16:15:56 +0200

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,23 @@
+# Ejercicios
+Nombre = 'Roberto'
+Apellidos = 'Carrillo Palma'
+Edad = 26
+Email = 'CarrilloRob@Palman.es'
+Telefono = 23658974
+Casado = True
+Con_Hijos = False
+ListaAmigos = ['Carlos', 'Juan', 'Mario', 'Arturo']
+PeliculasVistas = {
+... "1" : "Matrix",
+... "2" : "Jumper",
+... "3" : "Tiburon"
+... }
+
+print(Nombre)
+print(Apellidos)
+print(Edad)
+print(Email)
+print(Telefono)
+print(Con_Hijos)
+print(ListaAmigos)
+print(PelicilasVistas)
